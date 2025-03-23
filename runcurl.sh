1d727ba72efa3aa95b57588ecbc18d3c85e771c9
Stefan Nachtrab <stefan.nachtrab@adorsys.com>
2025-03-23 13:58:19 +0100

    back to self hosted solution for getting ip

--- runcurl.sh
+++ runcurl.sh
@@ -4,5 +4,5 @@ DATE_TIME=$(date '+%d/%m/%Y %H:%M:%S');
 
 echo "IP Update - $DATE_TIME"
 echo "User: $CURL_USERNAME"
-curl -s -u $CURL_USERNAME:$CURL_PASSWORD https://dyndns.kasserver.com/\?myip=`curl -s https://api.ipify.org`
+curl -s -u $CURL_USERNAME:$CURL_PASSWORD https://dyndns.kasserver.com/\?myip=`curl -s https://tools.steve4u.de/ip.php`
 echo -e "\n IP Update complete"
